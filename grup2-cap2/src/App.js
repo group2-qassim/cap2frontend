9fcf617a51450caebe842f5cf8dc87d9539b9e2a
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-11 14:18:31 +0300

    r

---
+++ grup2-cap2/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+
+function App() {
+  return (
+  <>
+ 
+  <div class="wrapper">
+        <div id="navbar">
+            <ul id="home">
+                <li><a href="ggggggggg" class="link-style">HOME</a></li>
+            </ul>
+            <img src="./logo country.png" alt="" id="image-nav" />
+
+            <ul id="fovarit">
+                <li id="favor"><a class="link-style" href="fffffff">FAVORITE</a></li>
+            </ul>
+        </div>
+
+        
+
+
+        
+
+    </div>
+
+    <div id="footer">
+        <h2>All  &copy; footer</h2>
+    </div>
+ 
+  </>
+  )}
+
+export default App;
