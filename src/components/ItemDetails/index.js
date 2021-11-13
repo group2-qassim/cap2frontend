0cf58a21562088e8cab506d0409460e78f84887c
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-13 06:19:00 +0300

    updeat

--- src/components/ItemDetails/index.js
+++ src/components/ItemDetails/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./style.css";
+import { AiOutlineHeart } from "react-icons/ai";
+
 
 const BASE_URL = "http://localhost:4000";
 
@@ -20,8 +23,52 @@ export default function ItemDetails() {
   };
 
   return (
-    <div>
-      <h1>ncx nnx</h1>
-    </div>
+    <section className="section-description">
+      <div className="container">
+        <div className="desc-grid">
+
+            
+          <div className="img-desc">
+          <div className="fav">
+              <button className="icon-btn">
+                <AiOutlineHeart />
+              </button>
+            </div>
+            <img src={item.artworkUrl100} />
+          </div>
+          <div className="content-desc">
+            <div className="item-desc">
+              <span className="title">{item.artistName}</span>
+            </div>
+            <div className="item-desc">
+            
+            <p>Kind: </p><span>{item.kind}</span>
+            </div>
+            <div className="item-desc">
+              <p>Collection Name:</p>
+              <span>{item.collectionName}</span>
+            </div>
+            <div className="item-desc">
+              <p>Price:</p>
+              <span>
+                {item.collectionPrice} {item.currency}
+              </span>
+            </div>
+            <div className="item-desc">
+              <p>Release date:</p>
+              <span>{item.releaseDate}</span>               
+            </div>
+            <div className="item-desc">
+              <p>Country:</p>
+              <span>{item.country}</span>
+            </div>
+            <div className="item-desc">
+              <p>Genres:</p>
+              <span>{item.genres}</span> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,6 +1,12 @@
 import { useParams, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./style.css";
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GiEarthAfricaEurope } from "react-icons/gi";
+import { GrCatalog } from "react-icons/gr";
+import { BsFillCalendarDateFill } from "react-icons/bs";
 
 const BASE_URL = "http://localhost:4000";
 
@@ -14,6 +20,7 @@ export default function Items() {
 
   const getAllItems = async () => {
     const items = await axios.get(`${BASE_URL}/${catg}`);
+    console.log(catg);
     setItems(items.data.results);
     console.log(items.data.results);
   };
@@ -26,19 +33,53 @@ export default function Items() {
   //
 
   return (
-    <div>
-      <ul>
+    <section className="section-items">
+      <div className="cards-box">
         {items.map((item) => (
-          <div
-            key={item.trackId}
-            onClick={() => {
-              goToItem(item);
-            }}
-          >
-            <h4>{item.artistName}</h4>
+          <div className="card">
+            <div className="fav">
+              <button className="icon-btn">
+                <AiOutlineHeart />
+              </button>
+            </div>
+            <div
+              key={item.trackId}
+              onClick={() => {
+                goToItem(item);
+              }}
+            >
+              <div className="content-box">
+                <p className="card-name">{item.artistName}</p>
+                <p className="content-item">
+                  <span>
+                    <GiEarthAfricaEurope /> Country:
+                  </span>{" "}
+                  <span>{item.country}</span>
+                </p>
+                <p className="content-item">
+                  <span>
+                    <GrCatalog /> Gernre:
+                  </span>{" "}
+                  <span>{item.primaryGenreName}</span>
+                </p>
+                <p className="content-item">
+                  <span>
+                    <BsFillCalendarDateFill /> release Date:
+                  </span>
+                  <span>
+                    {Intl.DateTimeFormat("en-us", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }).format(Date.parse(item.releaseDate))}
+                  </span>
+                </p>
+              </div>
+              <img className="card-img" src={item.artworkUrl100} />
+            </div>
           </div>
         ))}
-      </ul>
-    </div>
+      </div>
+    </section>
   );
 }
